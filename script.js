471f234360830c8e8cda2ae4ddfad1736086c46f
gerbelli27 <guerbell@hotmail.com>
2022-02-02 21:10:12 +0000

    color

---
+++ script.js
@@ -0,0 +1,32 @@
+function loadJSON(callback) {
+
+  var xobj = new XMLHttpRequest();
+  xobj.overrideMimeType("application/json");
+  xobj.open('GET', 'data.json', true);
+  xobj.onreadystatechange = function () {
+  if (xobj.readyState == 4 && xobj.status == "200") {
+  
+  // .open will NOT return a value but simply returns undefined in async mode so use a callback
+  callback(xobj.responseText);
+  
+  }
+  }
+  xobj.send(null);
+  
+  }
+  
+  // Call to function with anonymous callback
+  loadJSON(function(response) {
+  // Do Something with the response e.g.
+  jsonresponse = JSON.parse(response);
+  
+
+  console.log(jsonresponse);
+  
+  });
+
+
+  
+  const buttonDaily = document.getElementById('daily');
+  const buttonWekkly = document.getElementById('weekly');
+  const buttonMonthly = document.getElementById('monthly');
